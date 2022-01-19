b425be6fa36bb0747e56ae3522952870e0c6c972
lucas-nunesS <ns_lucas@hotmail.com>
2022-01-18 21:17:05 -0300

    refresh token in development

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: '975a1aa26a96ad7679b3aad6735dac5a',
+  expires_in_token: '15m',
+  secret_refresh_token: '54d96931cbbca2cbeb9489b8420c0d85',
+  expires_in_refresh_token: '30d',
+  expires_refresh_token_days: 30,
+};
