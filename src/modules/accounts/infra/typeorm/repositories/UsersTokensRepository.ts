dc84f88e463c46e5c3e915ebf2541c8d25738540
lucas-nunesS <ns_lucas@hotmail.com>
2022-01-19 21:00:41 -0300

    refresh token finished

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -27,6 +27,21 @@ class UsersTokensRepository implements IUsersTokensRepository {
 
     return userToken;
   }
+
+  async findByUserIdAndRefreshToken(
+    user_id: string,
+    refresh_token: string,
+  ): Promise<UserTokens> {
+    const users_tokens = await this.repository.findOne({
+      user_id,
+      refresh_token,
+    });
+
+    return users_tokens;
+  }
+  async deleteById(id: string): Promise<void> {
+    await this.repository.delete(id);
+  }
 }
 
 export { UsersTokensRepository };
